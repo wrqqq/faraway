f98c7a580848bada408908276211f6b879ce56bd
Il Sol <ilya.soldatenkov@in-aim.ru>
2022-07-19 16:45:25 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -17,26 +17,27 @@ function App() {
     const [characters, setCharacters] = useState([])
     const [characterName, setCharacterName] = useState("")
     axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded"
-    const getData = async () => {
-        /*axios
+
+    useEffect(() => {
+        const getData = async () => {
+            /*axios
             .get("http://developer.marvel.com/v1/public/comics?apikey=5bbb2895287d6a461a6e6419341fdf5a")
             .then((data) => {
                 console.log(data)
                 setPersons(data.data.results)
             })*/
 
-        const baseUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${characterName}`
-        const time = Number(new Date())
-        // eslint-disable-next-line no-useless-concat
-        const hash = md5(time + `fb2ee5e8ef44d3c6f0c7046db2e421d5d8181512` + `5bbb2895287d6a461a6e6419341fdf5a`)
+            const baseUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${characterName}`
+            const time = Number(new Date())
+            // eslint-disable-next-line no-useless-concat
+            const hash = md5(time + `fb2ee5e8ef44d3c6f0c7046db2e421d5d8181512` + `5bbb2895287d6a461a6e6419341fdf5a`)
 
-        const {data} = await axios.get(`${baseUrl}&ts=${time}&apikey=5bbb2895287d6a461a6e6419341fdf5a&hash=${hash}`)
-        setCharacters(data.data.results)
-    }
-
-    useEffect(() => {
+            const {data} = await axios.get(`${baseUrl}&ts=${time}&apikey=5bbb2895287d6a461a6e6419341fdf5a&hash=${hash}`)
+            setCharacters(data.data.results)
+            setCharacterName(data.data.results[0])
+        }
         getData()
-    }, [])
+    }, [characterName])
 
     return (
         <div className="App">
